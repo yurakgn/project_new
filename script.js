0c9d30793f6a98c248802cecfbd443cde07d2194
yurakgn@gmail.com <yuranet@inbox.ru>
2022-01-26 16:13:00 +0300

    0.02

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?')
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
 
 const personalMovieDB = {
-    count: 0,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-personalMovieDB.count = numberOfFilms;
+while (personalMovieDB.count != null) {
+    alert('Введите кол-во фильмов');
+    const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+}
+while (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+}
 
-const lostFilmShow = prompt('Какой последний фильм вы посмотрели:', '');
-const raitingFilms = +prompt('Оцените фильм от 0 до 10', '5');
-const lostFilmShow2 = prompt('Какой последний фильм вы посмотрели:', '');
-const raitingFilms2 = +prompt('Оцените фильм от 0 до 10', '5');
+if (personalMovieDB.count != null) {
+    alert('Введите кол-во фильмов');
+    const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+} else if(personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('error');
+}
+
+for (let i = 0; i < 2; i++) {
+    let films = prompt('Один из последних просмотренных фильмов?', '');
+    if (films = null && )
 
 
-personalMovieDB.movies[lostFilmShow] = raitingFilms;
-personalMovieDB.movies[lostFilmShow2] = raitingFilms2;
+    // while (films.length = NaN) {
+    //     alert('Вы не ввели название фильма');
+    //     films = prompt('Какой последний фильм вы посмотрели:', '');
+    // }
+    while (films.length <= 0) {
+        alert('Вы не ввели название фильма');
+        films = prompt('Какой последний фильм вы посмотрели:', '');
+    }
+    while (films.length > 10) {
+        alert('Вы ввели слишком длинное название');
+        films = prompt('Какой последний фильм вы посмотрели:', '');
+    }
+    let raitingFilms = +prompt('На сколько оцените его?', '5');
+    personalMovieDB.movies[films] = raitingFilms;
+}
+
 
 console.log(personalMovieDB)
-
-
