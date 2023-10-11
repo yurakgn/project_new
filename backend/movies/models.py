2ccad88b6f41193d1a2a3f891dcb8b51f1a44540
yurakgn <yurakgn@gmail.com>
2023-10-11 13:38:30 +0800

    add models movie

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+    categories = models.ManyToManyField(Category, related_name='movies')
+    genres = models.ManyToManyField(Genre, related_name='movies')
+    cover_image = models.ImageField(upload_to='movie_covers/')
+    trailer_url = models.URLField()
+
+    class Meta:
+        verbose_name = 'Movie'
+        verbose_name_plural = 'Movies'
+        ordering = ['-release_date']
+
+    def __str__(self):
+        return self.title
